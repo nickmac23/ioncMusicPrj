09ef8ad90e6193f65c2c6d244255af9ac80c0396
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-06-29 12:32:32 -0600

    set up global socket room variable in a facotry

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,7 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicSideMenuDelegate) {
+.factory('musicData', function () {
   var socketRoom;
+  return {
+    socketRoomSet,
+  }
+
+  function socketRoomSet (room){
+    if (room) socketRoom = room
+    return socketRoom
+  }
+})
+
+.controller('AppCtrl', function($scope, $ionicSideMenuDelegate, musicData) {
   var url = 'https://fathomless-falls-33454.herokuapp.com/'
   var socket = io.connect(url);
   $scope.setRoom = setRoom;
@@ -9,7 +20,7 @@ angular.module('starter.controllers', [])
   $scope.sendPath = sendPath;
 
   function setRoom(room) {
-    socketRoom = room
+    var socketRoom = musicData.socketRoomSet(room)
     socket.emit('server', {info: 'client wants data!', room: socketRoom, to: 'electron'})
     socket.on(socketRoom + 'client', function (data) {
       if (Array.isArray(data)) {
@@ -20,13 +31,13 @@ angular.module('starter.controllers', [])
           $scope.$apply()
         }
       } else {
-        console.log(data);
+        // console.log(data);
       }
     })
   }
 
   function sendPath (path) {
-    console.log(path);
+    var socketRoom = musicData.socketRoomSet()
     socket.emit('server', {info: path, room: socketRoom, to: 'electron'})
   }
 
